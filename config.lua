32df0d5245986ebd681ea97126fa73b0c3875f22
Core Rollespill <frank@corerp.org>
2025-10-23 22:22:56 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,10 @@
 Config = {}
 
-Config.BuyNewspaperText = 'Buy Newspaper' -- Text shown with qb-target
-Config.BuyNewspaperIcon = 'fas fa-newspaper' -- Icon shown with qb-target
-Config.Price = 100 -- Price of buying the newspaper
+--# Pris & Antall Database Handlinger
+Config.Price = 200 -- Price of buying the newspaper
 Config.AmountOfNews = 10 -- Amount of news to be fetched from the database
 Config.AmountOfSentences = 10 -- Amount of prison sentences to be fetched from the database
+
+--## Target
+Config.BuyNewspaperText = 'Kjøp Papiravis'
+Config.BuyNewspaperIcon = 'fas fa-newspaper'
